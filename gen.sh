5dc8e44892ae9df225759a17365e91f5a6d66447
wfhtqp <1006954237@qq.com>
2017-12-12 14:02:33 +0800

    fix

--- gen.sh
+++ gen.sh
@@ -3,5 +3,5 @@ gomobile bind -target=android -tags="mobile" github.com/nzlov/centrifuge-mobile
 gomobile bind -target=ios -tags="mobile" github.com/nzlov/centrifuge-mobile
 cp centrifuge.aar examples/android/CentrifugoAndroid/app/libs/centrifuge.aar
 cp centrifuge.aar examples/android/CentrifugoAndroid/centrifuge/centrifuge.aar
-cp -a Centrifuge.framework examples/ios-oc/CentrifugoIOS/Centrifuge.framework
-cp -a Centrifuge.framework examples/ios-swift/CentrifugoIOS/Centrifuge.framework
+cp -a Centrifuge.framework examples/ios-oc/CentrifugoIOS/
+cp -a Centrifuge.framework examples/ios-swift/CentrifugoIOS/
